996dba3e68112bc246b6c05fbfce13d3478ecdb5
Yessenia Vela <vela.kathy@gmail.com>
2021-10-06 14:00:26 -0500

    adiciona path vacío para login e importa/exporta reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class AppRoutingModule { }
