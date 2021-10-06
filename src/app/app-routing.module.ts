62840491b890ee3001f084aa9d8480c5fb7bfc1e
Yessenia Vela <vela.kathy@gmail.com>
2021-10-06 18:51:42 -0500

    add path for chatboxcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { ChatBoxComponent } from './components/chat-box/chat-box.component';
 
 const routes: Routes = [
   {
     path:'',
     component: LoginComponent
+  },
+  {
+    path:'chat',
+    component: ChatBoxComponent
   }
 ];
 
