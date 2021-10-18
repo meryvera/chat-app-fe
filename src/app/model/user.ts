e564ce5f1cdd8d8b2bf4e4b73a8c91bd042b79f4
Yessenia Vela <vela.kathy@gmail.com>
2021-10-18 11:55:44 -0500

    add token & auth model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -3,3 +3,12 @@ export interface UserModel {
     name: string,
     password?: string,
 }
+
+export interface AuthModel {
+    email?: string,
+    password?: string,
+}
+
+export interface Token {
+    token: string,
+}
