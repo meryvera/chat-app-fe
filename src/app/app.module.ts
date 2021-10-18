27b8e990b0f1d7892ea1fc6ee34fb4616a1d1804
YessVe <77126999+YessVe@users.noreply.github.com>
2021-10-18 16:32:57 -0500

    Revert "Front yess"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ChatBoxComponent } from './components/view general/chat-box/chat-box.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignupComponent } from './components/signup/signup.component';
-import { ContainerChannelComponent } from './components/view general/container-channel/container-channel.component';
-import { ContainerUsersComponent } from './components/view general/container-users/container-users.component';
-import { ViewGeneralComponent } from './components/view general/view-general/view-general.component';
-
-//import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
-//const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ChatBoxComponent,
-    SignupComponent,
-    ContainerChannelComponent,
-    ContainerUsersComponent,
-    ViewGeneralComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
-    //SocketIoModule.forRoot(config)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
